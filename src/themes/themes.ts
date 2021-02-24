2129855f86f508d89437b648738ed124181d6919
kritzona <virell.m@yandex.ru>
2021-02-24 18:48:05 +0300

    Подключил светлую тему и темную в общем объекте тем

---
+++ src/themes/themes.ts
@@ -0,0 +1,9 @@
+import { DarkTheme } from './DarkTheme/DarkTheme'
+import { LightTheme } from './LightTheme/LightTheme'
+
+const themes = {
+  dark: DarkTheme,
+  light: LightTheme,
+}
+
+export default themes
